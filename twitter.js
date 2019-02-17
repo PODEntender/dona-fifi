2edaa6dd96ddcb300e11b5e85d9b66c089bd5dc6
Nawarian <nickolas@phpsp.org.br>
2019-02-17 13:03:49 +0100

    Adjusting data

--- twitter.js
+++ twitter.js
@@ -10,9 +10,11 @@ http.get('https://twitter.com/podentender')
 .then((res) => {
   const $ = require('cheerio').load(res.data);
 
-  const tweets = $('a[data-nav="tweets"] [data-count]').html().trim().replace('.', '');
-  const followers = $('a[data-nav="followers"] [data-count]').html().trim().replace('.', '');
-  const following = $('a[data-nav="following"] [data-count]').html().trim().replace('.', '');
+  const toInt = (input) => parseInt(input.html().trim().replace('.', '').replace(',', ''));
+
+  const tweets = toInt($('a[data-nav="tweets"] [data-count]'));
+  const followers = toInt($('a[data-nav="followers"] [data-count]'))
+  const following = toInt($('a[data-nav="following"] [data-count]'))
   const date = moment().format('YYYY-MM-DD');
 
   console.log(`"Twitter;"${date}","${followers}";"${following}";"${tweets}"`);
